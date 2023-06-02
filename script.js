d91d172e5aea7cb3b712677315c58ce9761c08da
Simran Mandal <simranmandal0211@gmail.com>
2023-06-02 17:30:54 +0530

    Add script and initialize some references

---
+++ script.js
@@ -0,0 +1,29 @@
+// Initial References
+let timeRef = document.querySelector('.timer-display');
+const hourInput = document.getElementById('hourInput');
+const minuteInput = document.getElementById('minuteInput');
+const activeAlarms = document.querySelector('.activeAlarms');
+const setAlarm = document.getElementById('set');
+
+let alarmsArray = [];
+let alarmSound = new Audio('alarmClock.mp3');
+
+let initialHour = 0,
+    initialMinute = 0,
+    alarmIndex = 0;
+
+// Append zeros for single digit
+
+// Search for value in object
+
+// Display Time
+
+// Create alarm Div
+
+// Set Alarm
+
+// Start Alarm
+
+// Stop Alarm
+
+// Delete alarm
