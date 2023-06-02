fb8d860c8af71ea5adec8b34854927a61ad2b854
Simran Mandal <simranmandal0211@gmail.com>
2023-06-02 17:35:16 +0530

    Add functions for appendZero(), inputCheck() and window.onlod()

--- script.js
+++ script.js
@@ -13,11 +13,30 @@ let initialHour = 0,
     alarmIndex = 0;
 
 // Append zeros for single digit
+const appendZero = (value) => (value < 10 ? "0" + value : value);
 
 // Search for value in object
 
 // Display Time
+function displayTimer(){
 
+}
+
+const inputCheck = (inputValue) => {
+    inputValue = parseInt(inputValue);
+    if(inputValue < 10){
+        inputValue = appendZero(inputValue);
+    }
+    return inputValue
+};
+
+hourInput.addEventListener('input', () => {
+    hourInput.value = inputCheck(hourInput.value);
+});
+
+minuteInput.addEventListener('input', () => {
+    minuteInput.value = inputCheck(minuteInput.value);
+});
 // Create alarm Div
 
 // Set Alarm
@@ -27,3 +46,14 @@ let initialHour = 0,
 // Stop Alarm
 
 // Delete alarm
+
+window.onload = () => {
+    setInterval(displayTimer);
+    initialHour = 0;
+    initialMinute = 0;
+    alarmIndex = 0;
+
+    alarmsArray = [];
+    hourInput.value = appendZero(initialHour);
+    minuteInput.value = appendZero(initialMinute);
+};
